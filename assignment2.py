f41197fad79571216d6dbc0917307151aa070338
Jose Mayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-09-16 17:03:28 +0200

    assignment2

--- assignment2.py
+++ assignment2.py
@@ -8,25 +8,44 @@ options['OBSTACLES'] = False
 
 class ModelPredictiveControl:
     def __init__(self):
-        self.horizon = 2
+        self.horizon = 20
         self.dt = 0.2
 
         # Reference or set point the controller will achieve.
         self.reference1 = [10, 10, 0]
-        self.reference2 = None #[10, 2, 3.14/2]
+        self.reference2 = [10, 2, 3*3.14/2]
 
     def plant_model(self,prev_state, dt, pedal, steering):
         x_t = prev_state[0]
         y_t = prev_state[1]
         psi_t = prev_state[2]
         v_t = prev_state[3]
+        x_t_1 = x_t + v_t * np.cos(psi_t) * dt
+        y_t_1 = y_t + v_t * np.sin(psi_t) * dt
+        v_t_1 = v_t + pedal*dt - v_t/25.0# m/s
+        
+        L=1.0
+        R = L/np.tan(steering)
+        R=2.5 #HACK according to the instructions
+        psi_t_1 = psi_t + v_t* np.tan(steering)/R
 
-        return [x_t, y_t, psi_t, v_t]
+        return [x_t_1, y_t_1, psi_t_1, v_t_1]
 
     def cost_function(self,u, *args):
         state = args[0]
         ref = args[1]
         cost = 0.0
+        
+        for k in range(0,self.horizon):
+            vt_ref = state[3]
+            state = self.plant_model(state, self.dt, u[k*2],u[k*2+1])
+            cost += 1.0*abs(ref[0] - state[0])**2
+            cost += 1.0*abs(ref[1] - state[1])**2
+            cost += 1.0*abs(ref[2] - state[2])**2
+            #Limit Speed change
+            cost += 100.0*abs(vt_ref - state[3])**2
+            #LIMIT STEERING
+            cost += 2.0*u[k*2+1]**2
         return cost
 
 sim_run(options, ModelPredictiveControl)
